2e00805bbaa46e3057b29e4a81a54dcc1e0aaec8
rfunk82 <rfunk82@gmail.com>
2014-01-24 16:14:20 -0500

    Initial Commit

---
+++ RFFileDownload/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RFFileDownload
+//
+//  Created by Ricardo Funk on 1/24/14.
+//  Copyright (c) 2014 Ricardo Funk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
